da9bfcc86835e2d91d550d7f60cd1a4d5f0b851d
Jas Der <jasmineder@gmail.com>
2016-02-03 17:39:29 +0000

    admin routing working so far

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,21 @@
 class Admin::UsersController < ApplicationController
 
-  def index
-    render text: "Index for Admin"
-  end
+  # def index
+
+  # end
 
   def new
     @admin_user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
   end
 end
